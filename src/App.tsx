47cabd17c2a82ae64150e87e3a22c546feec2c5f
jiren <you@example.com>
2025-08-22 05:05:51 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import BTTH from "./components/BTTH/BTTH";
 import Ex5 from "./components/PTIT_CNTT4_IT104_SESSION014_EXERCISE05/Ex5";
 import Ex6 from "./components/PTIT_CNTT4_IT104_SESSION014_EXERCISE06/Ex6";
 import Ex7 from "./components/PTIT_CNTT4_IT104_SESSION014_EXERCISE07/Ex7";
@@ -10,6 +11,7 @@ function App() {
       {/* <Ex6 /> */}
       {/* <Ex7 /> */}
       <Ex8 />
+      {/* <BTTH /> */}
     </>
   );
 }
